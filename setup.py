2ea54f0be56ecf76e80c3053d7ec18a98c9a6ecc
Talley Lambert <tlambert03@gmail.com>
2015-03-09 20:36:35 -0400

    test commit

--- setup.py
+++ setup.py
@@ -4,14 +4,13 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'My Project',
-    'author': 'My Name',
-    'url': 'URL to get it at.',
+    'description': 'Group Optimization Algorithm',
+    'author': 'Talley Lambert',
     'download_url': 'Where to download it.',
     'author_email': 'My email.',
     'version': '0.1',
     'install_requires': ['nose'],
-    'packages': ['NAME'],
+    'packages': ['grouper'],
     'scripts': [],
     'name': 'projectname'
 }
