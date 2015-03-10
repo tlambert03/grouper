37598a87d6ac28a8a3457c875a4f438a03d334bb
Talley Lambert <tlambert03@gmail.com>
2015-03-09 20:38:01 -0400

    added config to setup

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ except ImportError:
 config = {
     'description': 'Group Optimization Algorithm',
     'author': 'Talley Lambert',
-    'download_url': 'Where to download it.',
-    'author_email': 'My email.',
+    'download_url': 'https://github.com/tlambert03/grouper.git',
+    'author_email': 'tlambert03@gmail.com',
     'version': '0.1',
     'install_requires': ['nose'],
     'packages': ['grouper'],
     'scripts': [],
-    'name': 'projectname'
+    'name': 'Grouper'
 }
 
 setup(**config)
